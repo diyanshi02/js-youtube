f61ca24458931bbe7c78370372299d7c151071aa
diyanshi02 <94775794+diyanshi02@users.noreply.github.com>
2025-08-22 05:44:36 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as the newer version
+
+//alert( 3 + 3) // we are using nodejs not browser
+
+console.log(3+3); //code readabiity is important
+console.log("diyanshi")
+
+let name = "diyanshi"
+let age = 24
+let isLoggedIn = false
+let state;
+
+// number => 2 to the power of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); //undefiend
+console.log(typeof null); // object
