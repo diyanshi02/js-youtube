973c80e8f3d947a18d04cba4f2a4d559822a1ca6
diyanshi02 <94775794+diyanshi02@users.noreply.github.com>
2025-08-24 14:11:11 +0000

    yo

---
+++ .devcontainer/03_basics/01_functions.js
@@ -0,0 +1,55 @@
+// console.log("D")
+// console.log("I")
+// console.log("y")
+// console.log("a")
+
+
+function sayMyName(){
+    console.log("D")
+console.log("I")
+console.log("y")
+console.log("a")
+
+}
+//sayMyName()
+
+// function addTwoNumbers(num1,num2){
+
+//     console.log(num1 + num2)
+// }
+
+function addTwoNumbers(num1,num2){
+
+    // let result = num1 + num2
+    // return result
+
+    return num1 + num2
+
+}
+const result = addTwoNumbers(4,6)
+
+//console.log("Result:", result);
+
+function loginUserMessage(username){
+    if(!username){
+        console.log("please give a username")
+        return
+
+    }
+    return `${username} just logged in`
+}
+//console.log(loginUserMessage("diya"))
+//console.log(loginUserMessage()) //if no vaue passed it gives undefined
+
+function calculateCartPrice(val1, val2, ...num1){   //REST operator
+return num1
+}
+console.log(calculateCartPrice(200,300,400))
+
+const user ={
+    username: "diya",
+    price: 87
+}
+function handleObject(anyObject){
+    console.log(`username is ${anyObject.username} and price is${anyObject.price}`)
+}
